23a339a3728bf89146ab3b1edc3fd68353089a88
github username <github email address>
2018-12-31 10:09:30 -0600

    add solutions to reverse string exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let string = str.split('')
+  return string.reverse().join('')
+}
+
+function reverse(str) {
+  let reversed = '';
+  for (let character of str){
+   reversed = character + reversed;
+  }
+  return reversed
+
+}
+
+function reverse(str) {
+  return str.split('').reduce((accumulator, current) => current + accumulator , '')
+}
+
 
 module.exports = reverse;
