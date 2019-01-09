7039e21ad78bbd4f2c85716b44ef4bcfed7ead30
github username <github email address>
2019-01-09 09:20:34 -0600

    add alternate maxchar question

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -38,6 +38,75 @@ maxChar('Hello World!')
 //     return chars
 // }
 
+
+//Word Sorter
+// Given a list of 11 words, we want to find the n-th most occurring word(s).
+//
+// Example given n=2, we want to find the 2nd most occurring word(s)
+//
+// In case of ties, return both
+// We also want to optimize for reads of n
+
+var words = ['a', 'b', 'c', 'd', 'a', 'b', 'c', 'a', 'b', 'c', 'a']
+
+function countWords( arr) {
+  let chars = {}
+  let charValues;
+
+   for ( let char of arr) {
+    chars[char] = chars[char] + 1 || 1;
+   }
+
+return chars
+}
+
+// chars = { a: 4, b: 3, c: 3, d: 1 }
+
+let wordCounts = countWords(words)
+let nthWords = []
+
+function findNth(freq){
+  for (const [key, val] of Object.entries(wordCounts)) {
+    if (val == freq) {
+      nthWords.push(key);
+    }
+  }
+  return nthWords
+}
+
+findNth(1); // returns 'd'
+findNth(4); // returns 'a'
+
+//other way
+
+var words = ['a', 'b', 'c', 'd', 'a', 'b', 'c', 'a', 'b', 'c', 'a']
+
+function countWords( arr) {
+  let chars = {}
+  let charValues;
+
+   for ( let char of arr) {
+    chars[char] = chars[char] + 1 || 1;
+   }
+
+return chars
+}
+
+// chars = { a: 4, b: 3, c: 3, d: 1 }
+
+let wordCounts = countWords(words)
+let nthWords = []
+
+function findNth(freq){
+  for ( let char in wordCounts){
+    if (wordCounts[char] == freq) {
+      nthWords.push(char);
+    }
+  }
+  return nthWords
+}
+
+
 module.exports = maxChar;
 
 //any problem where you are counting the number of characters ------use this technique
