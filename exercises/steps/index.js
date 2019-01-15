65f15033533acdd83e2d07ae7c19cb1a6b4a6a38
github username <github email address>
2019-01-14 18:21:15 -0600

    solution 1, step problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,22 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//solution 1
+function steps(n) {
+
+
+  for ( let row = 0; row < n; row++) {
+    let stair = '';
+    for (let column = 0; column < n; column++){
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+
+}
 
 module.exports = steps;
