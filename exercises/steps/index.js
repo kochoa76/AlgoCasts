a7abeb02c965ef982e8adecbcc24f6310686c46c
github username <github email address>
2019-01-18 15:40:10 -0600

    adding steps problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -20,19 +20,18 @@
 //solution 1
 function steps(n) {
 
-
-  for ( let row = 0; row < n; row++) {
-    let stair = '';
-    for (let column = 0; column < n; column++){
-      if (column <= row) {
-        stair += '#';
-      } else {
-        stair += ' ';
-      }
+for (let row = 0; row < n; row++) {
+  let stair = '';
+  for ( let column = 0; column < n; column++) {
+    if ( column <= row ) {
+      stair += '#';
+    } else {
+      stair += ' ';
     }
-    console.log(stair);
+  }
+    console.log(stair)
+    }
   }
 
-}
 
 module.exports = steps;
